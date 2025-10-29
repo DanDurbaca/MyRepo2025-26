6a8684224e2360a2bfbbe7c0b23660d4f9e71d00
Dude <dd@lpem.com>
2025-10-29 14:15:09 +0100

    jq

--- WEBAP/MyOwnJs.js
+++ WEBAP/MyOwnJs.js
@@ -1,7 +1,10 @@
-function changeText() {
+$(function () {
+    $("#ChangeThis").html("Hello, World!");
+});
+
+/*function changeText() {
     document.getElementById("ChangeThis").innerHTML = "Hello, World!";
 }
 
 document.addEventListener("DOMContentLoaded", changeText);
-
-
+*/
